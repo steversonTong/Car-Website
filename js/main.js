7c0429b32f050726c99fc37c3e9c5dc031a9276a
Steve Tong <stevetong53@yahoo.com>
2022-03-28 21:01:20 -0700

    added swiper function for car cards

--- js/main.js
+++ js/main.js
@@ -31,3 +31,22 @@ function scrollHeader() {
   }
 }
 window.addEventListener("scroll", scrollHeader);
+
+let swiperPopular = new Swiper(".popular_container", {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: "auto",
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      spaceBetween: 48,
+    },
+  },
+});
