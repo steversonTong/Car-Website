9026c8a06b7c8f6487d4b84dd7969a8cee987eb1
Steve Tong <stevetong53@yahoo.com>
2022-03-30 01:42:25 -0700

    added scroll animation

--- js/main.js
+++ js/main.js
@@ -96,3 +96,22 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".home_title, .popular_container, .features_img, .featured_filters");
+sr.reveal(".home_subtitle", { delay: 500 });
+sr.reveal(".home_elec", { delay: 600 });
+sr.reveal(".home_img", { delay: 800 });
+sr.reveal(".home_car-data", { delay: 900, interval: 100, origin: "bottom" });
+sr.reveal(".home_button", { delay: 0100, origin: "bottom" });
+sr.reveal(".about_group, .offer_data", { origin: "left" });
+sr.reveal(".about_data, .offer_img", { origin: "right" });
+sr.reveal(".features_map", { delay: 600, origin: "bottom" });
+sr.reveal(".features_card", { interval: 300 });
+sr.reveal(".featured_card, .logos_content, .footer_content", { interval: 100 });
